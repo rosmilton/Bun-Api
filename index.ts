db1c0d2f36a62bc3938b9015f0b190839fe5d3fe
Rosmilton <rosmilton@gmail.com>
2026-02-22 01:32:26 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,43 @@
+import { cerebrasService } from './services/cerebras';
+import { groqService } from './services/groq';
+import type { AIService, ChatMessage } from './types';
+
+const services: AIService[] = [
+  groqService,
+  cerebrasService
+]
+
+let currentServiceIndex = 0;
+
+function getNextService() {
+  const service = services[currentServiceIndex];
+  currentServiceIndex = (currentServiceIndex + 1) % services.length;
+  return service;
+}
+
+const server = Bun.serve({
+  port: process.env.PORT ?? 3000,
+  async fetch(req) {
+    const { pathname } = new URL(req.url);
+
+    if (req.method === 'POST' && pathname === '/chat') {
+      const { messages } = await req.json() as { messages: ChatMessage[] };
+      const service = getNextService();
+
+      console.log(`Using ${service?.name} service`);
+      const stream = await service?.chat(messages);
+
+     return new Response(stream, {
+        headers: {
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          'Connection': 'keep-alive',
+        },
+      });
+    }
+
+    return new Response("Not found", { status: 404 });
+  }
+})
+
+console.log(`Server is running on ${server.url}:${server.port}`);
